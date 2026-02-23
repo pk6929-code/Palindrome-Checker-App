e7181b983ef4b051f71ae451c69be9bfd0d75748
pk6929-code <pk6929@srmist.edu.in>
2026-02-23 15:48:53 +0530

    UC2: Print a Hardcoded Palindrome Result

---
+++ Palindrome Checker App/src/PalindromeCheck.java
@@ -0,0 +1,22 @@
+public class PalindromeCheck {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check if palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println("The string \"" + word + "\" is a palindrome.");
+        } else {
+            System.out.println("The string \"" + word + "\" is not a palindrome.");
+        }
+    }
+}
